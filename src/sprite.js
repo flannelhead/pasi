45b25c62750cea6c8d3b7cf1c7fe6d4d8ba23c55
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2014-12-28 16:51:18 +0200

    Remove unused code

--- src/sprite.js
+++ src/sprite.js
@@ -6,42 +6,7 @@ function SpriteSheet(image, nSprites) {
 }
 
 SpriteSheet.prototype.draw = function(ctx, x, y, index) {
-    console.log(x + ' ' + y);
     ctx.drawImage(this.image, index * this.spriteWidth, 0, this.spriteWidth,
         this.spriteHeight, x, y, this.spriteWidth, this.spriteHeight);
 };
-
-function spriteSheet(image, n) {
-    var width = image.width / n;
-    return {
-        image: image,
-        nFrames: n,
-        frameWidth: width,
-        frameHeight: image.height,
-    };
-}
-
-function drawSpriteObject(ctx, object, offset) {
-    if (!offset) offset = 0;
-    var sheet = object.sheet;
-    ctx.drawImage(object.sheet.image,
-        (offset + object.animIndex) * sheet.frameWidth, 0,
-        sheet.frameWidth, sheet.frameHeight,
-        object.x, object.y,
-        sheet.frameWidth, sheet.frameHeight
-    );
-}
-
-function tickSpriteObject(object) {
-    if (++object.tick === object.animTicks) {
-        animateSpriteObject(object);
-        object.tick = 0;
-    }
-}
-
-function animateSpriteObject(object) {
-    if (++object.animIndex === object.animLength) {
-        object.animIndex = 0;
-    }
-}
 
