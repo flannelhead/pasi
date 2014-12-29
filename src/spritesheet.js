7bc94eb56b18056486c429fcc6be1335c8ffa683
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2014-12-29 14:10:51 +0200

    Add missing spritesheet.js

---
+++ src/spritesheet.js
@@ -0,0 +1,12 @@
+function SpriteSheet(image, nSprites) {
+    this.image = image;
+    this.nSprites = nSprites;
+    this.spriteWidth = image.width / nSprites;
+    this.spriteHeight = image.height;
+}
+
+SpriteSheet.prototype.drawSprite = function(ctx, x, y, index) {
+    ctx.drawImage(this.image, index * this.spriteWidth, 0, this.spriteWidth,
+        this.spriteHeight, x, y, this.spriteWidth, this.spriteHeight);
+};
+
