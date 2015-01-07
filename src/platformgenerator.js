9a3fb31ecd0520a0f729c2208cf98dbf8c2e21c2
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2015-01-08 00:00:15 +0200

    The platforms must completely fit in the width.

--- src/platformgenerator.js
+++ src/platformgenerator.js
@@ -20,7 +20,8 @@ PlatformGenerator.prototype.generatePlatforms = function(yLower, yUpper) {
         var toss = Math.random() < 0.5 ? -1 : 1,
             dx = toss * (this.xSpaceMin + this.xSpaceRandom * Math.random()),
             dy = this.ySpaceMin + this.ySpaceRandom * Math.random();
-        var x = Math.min(Math.max(highestPlatform.x + dx, 0), this.width);
+        var x = Math.min(Math.max(highestPlatform.x + dx,
+            highestPlatform.width / 2), this.width - highestPlatform.width / 2);
         highestPlatform = new Platform(Math.round(x),
             Math.round(highestPlatform.y - dy));
         newPlatforms.push(highestPlatform);
