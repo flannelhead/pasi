6463a3cbf4701ae7adbaf40f7ec0cf7c471f885d
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2015-01-11 02:00:52 +0200

    Few more corrections and anchoring.

--- src/spritefont.js
+++ src/spritefont.js
@@ -23,21 +23,31 @@ SpriteFont.prototype.recolour = function(colour) {
             newData[i] = colour.R;
             newData[i + 1] = colour.G;
             newData[i + 2] = colour.B;
-            newData[i + 3] = this.refData[i];
+            newData[i + 3] = this.refData[i + 3];
         }
     }
 
     this.ctx.putImageData(newImgData, 0, 0);
 };
 
-SpriteFont.prototype.drawText = function(ctx, x, y, text) {
+SpriteFont.prototype.drawText = function(ctx, x, y, text, anchor) {
     var len = text.length, idx;
+    if (!anchor) anchor = 'l';
+    var anchorLower = anchor.toLowerCase();
+    var startX = x;
+    if (anchor === 'r') {
+        startX -= len * this.charWidth;
+    } else if (anchor === 'c') {
+        startX -= Math.round(len * this.charWidth / 2);
+    }
+
     for (var i = 0; i < len; ++i) {
         idx = this.charmap.indexOf(text[i]);
         if (idx !== -1) {
             ctx.drawImage(this.charSheet, idx * this.charWidth, 0,
                 this.charWidth, this.charHeight,
-                x + i * this.charWidth, y, this.charWidth, this.charHeight);
+                startX + i * this.charWidth, y,
+                this.charWidth, this.charHeight);
         }
     }
 };
