8df9a477f43488a35435e707536fa0eb720e83e5
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2015-01-17 00:51:08 +0200

    Remove obsolete code.

--- src/platform.js
+++ src/platform.js
@@ -10,11 +10,5 @@ function Platform(image, x, y) {
 Platform.prototype.draw = function(ctx) {
     ctx.drawImage(this.image, 0, 0, this.imageWidth, this.imageHeight,
         this.x - this.imageWidth / 2, this.y, this.imageWidth, this.imageHeight);
-    // ctx.strokeStyle = 'blue';
-    // ctx.lineWidth = 2;
-    // ctx.beginPath();
-    // ctx.moveTo(this.x - this.width / 2, this.y + 1);
-    // ctx.lineTo(this.x + this.width / 2, this.y + 1);
-    // ctx.stroke();
 };
 
