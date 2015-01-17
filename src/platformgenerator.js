cbab459ffd459affd7dff265489e1d3281c5be5b
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2015-01-17 22:47:39 +0200

    Tune the platform spacing algorithm

--- src/platformgenerator.js
+++ src/platformgenerator.js
@@ -1,26 +1,32 @@
 function PlatformGenerator(branchSheet, width, xFirst, yFirst) {
     this.width = width;
 
-    this.xSpaceMin = 30;
-    this.xSpaceRandom = 30;
-    this.ySpaceMin = 15;
-    this.ySpaceRandom = 60;
+    this.xSpaceMin = 20;
+    this.xSpaceRandom = 90;
+    this.ySpaceMin = 10;
+    this.ySpaceMax = 60;
+    this.spacingFactor = 0.02;
 
     this.branchSheet = branchSheet;
     this.platforms = [new Platform(branchSheet, xFirst, yFirst)];
+    this.highestPlatform = this.platforms[0];
 }
 
 PlatformGenerator.prototype.generatePlatforms = function(yLower, yUpper) {
-    var highestPlatform = this.platforms[this.platforms.length - 1];
-
     var newPlatforms = this.platforms.filter(function(platform) {
         return platform.y <= yLower;
     });
 
+    var highestPlatform = this.highestPlatform;
+    var ySpaceRandom = Math.min(Math.max(this.spacingFactor * Math.abs(yLower),
+        1), this.ySpaceMax - this.ySpaceMin);
     while (highestPlatform.y > yUpper) {
-        var toss = Math.random() < 0.5 ? -1 : 1,
-            dx = toss * (this.xSpaceMin + this.xSpaceRandom * Math.random()),
-            dy = this.ySpaceMin + this.ySpaceRandom * Math.random();
+        var toss;
+        if (highestPlatform.x < this.xSpaceMin) toss = 1;
+        else if (highestPlatform.x > this.width - this.xSpaceMin) toss = -1;
+        else toss = Math.random() < 0.5 ? -1 : 1;
+        var dx = toss * (this.xSpaceMin + this.xSpaceRandom * Math.random()),
+            dy = this.ySpaceMin + ySpaceRandom * Math.random();
         var x = Math.min(Math.max(highestPlatform.x + dx,
             highestPlatform.width / 2), this.width - highestPlatform.width / 2);
         highestPlatform = new Platform(this.branchSheet, Math.round(x),
@@ -29,5 +35,6 @@ PlatformGenerator.prototype.generatePlatforms = function(yLower, yUpper) {
     }
 
     this.platforms = newPlatforms;
+    this.highestPlatform = highestPlatform;
 };
 
