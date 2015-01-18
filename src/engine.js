45589bb8f2359a25be08011dcc470a0ceef941da
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2015-01-18 16:02:53 +0200

    Remove resize function from game engine.

--- src/engine.js
+++ src/engine.js
@@ -4,31 +4,6 @@ function startGame(canvas, config, draw, update, globalHandlers,
     var ctx = canvas.getContext('2d'),
         container = canvas.parentNode;
 
-    function setupCanvas() {
-        var w = container.offsetWidth,
-            h = container.offsetHeight,
-            m = Math.floor(w / config.width),
-            n = Math.floor(h / config.height),
-            scale = Math.max(Math.min(m, n), 1);
-
-        var width = config.width * scale,
-            height = config.height * scale;
-
-        canvas.width = width;
-        canvas.height = height;
-        canvas.style.width = width + 'px';
-        canvas.style.height = height + 'px';
-
-        ctx.imageSmoothingEnabled = false;
-        ctx.mozImageSmoothingEnabled = false;
-        ctx.webkitImageSmoothingEnabled = false;
-        ctx.msImageSmoothingEnabled = false;
-
-        ctx.scale(scale, scale);
-    }
-    // setupCanvas();
-    // window.addEventListener('resize', setupCanvas);
-
     var world = initialWorld;
 
     globalHandlers.forEach(function(handler) {
